c50e36063d7dccd87d7085902928854541bc105d
sibenshtern <sibenshtern@gmail.com>
2020-03-08 09:26:24 +0300

    fix error in index.py

--- index.py
+++ index.py
@@ -5,8 +5,8 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/<str:title>')
-@app.route('/index/<str:title>')
+@app.route('/<string:title>')
+@app.route('/index/<string:title>')
 def index(title):
     return render_template("base.html", title=title)
 
