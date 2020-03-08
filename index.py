3735eaa1ca985886ca5a0ec99a4de28fdd146e5b
sibenshtern <sibenshtern@gmail.com>
2020-03-08 21:16:31 +0300

    add login page and change design

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
+
+from loginform import LoginForm
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "veryverysecretkeywhichyoucantbreak"
 
 
 @app.route('/<string:title>')
@@ -15,6 +18,16 @@ def index(title):
 def show_professions_list(list_type):
     return render_template("professions_list.html", list_type=list_type,
                            title="Список профессий")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return redirect('/success')
+
+    return render_template("login.html", title="Авторизация", form=form)
 
 
 if __name__ == '__main__':
