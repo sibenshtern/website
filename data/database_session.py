42d4e8709eb2044f7e68693dbdb5b0b486537f1a
sibenshtern <sibenshtern@gmail.com>
2020-05-09 22:15:43 +0300

    Обновил дизайн и добавил страничку /distribution

--- data/database_session.py
+++ data/database_session.py
@@ -3,22 +3,24 @@ import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
 
-
 SqlAlchemyBase = dec.declarative_base()
+
 __factory = None
 
 
-def global_init(database_file):
+def global_init(db_file):
     global __factory
 
     if __factory:
         return
 
-    if not database_file or not database_file.strip():
+    if not db_file or not db_file.strip():
         raise Exception('Укажите имя файла БД')
 
-    connection_str = f"sqlite:///{database_file.strip()}?check_same_thread=False"
-    engine = sa.create_engine(connection_str)
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    print('Подключимся к БД по адресу:', conn_str)
+
+    engine = sa.create_engine(conn_str, echo=True)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
