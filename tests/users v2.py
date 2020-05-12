a317abbbdfba826e4020c9e6db27dc8d5c32ed63
sibenshtern <sibenshtern@gmail.com>
2020-05-13 02:45:55 +0300

    add tests for new User API

---
+++ tests/users v2.py
@@ -0,0 +1,20 @@
+import requests
+
+json = {
+    "name": "Egor",
+    "surname": "Ivanov",
+    "email": "tyaplyap@gmail.com",
+    "age": "15",
+    "position": "Inhabitant",
+    "speciality": "",
+    "password": "NONONO",
+    "address": "module_2"
+}
+
+print(requests.get("http://127.0.0.1:5000/api/v2/users/1").json())
+print(requests.get("http://127.0.0.1:5000/api/v2/users/123").json())
+
+requests.delete("http://127.0.0.1:5000/api/v2/users/1")
+
+requests.post("http://127.0.0.1:5000/api/v2/users", json=json)
+print(requests.get("http://127.0.0.1:5000/api/v2/users").json())
